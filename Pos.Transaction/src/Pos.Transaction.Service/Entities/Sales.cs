27379392bf4c4db8ebc65712f47e0006ab061e80
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-04-04 20:34:09 +0700

    Update Sales.cs

--- Pos.Transaction/src/Pos.Transaction.Service/Entities/Sales.cs
+++ Pos.Transaction/src/Pos.Transaction.Service/Entities/Sales.cs
@@ -9,9 +9,8 @@ namespace Pos.Transaction.Service.Entities
     public class Sales : IEntity
     {
         public Guid Id { get; init; }
-        public string Name { get; set; } = "";
-        public string ContactNumber { get; set; } = "";
-        public string Address { get; set; } = "";
-        public string Email { get; set; } = "";
+        public Guid CustomerId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal TotalAmount { get; set; }
     }
 }
